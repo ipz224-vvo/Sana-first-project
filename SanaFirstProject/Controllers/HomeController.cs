a30728d44481ed3a4aea1e216515e07b6cacc933
Viacheslav Varnavskyi <ipz224_vvo@student.ztu.edu.ua>
2023-03-26 23:11:19 +0300

    Add project files.

---
+++ SanaFirstProject/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SanaFirstProject.Models;
+using System.Diagnostics;
+
+namespace SanaFirstProject.Controllers
+{
+	public class HomeController : Controller
+	{
+		
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index(User user)
+		{
+			return View(user);
+		}
+
+		
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ SanaFirstProject/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace SanaFirstProject.Models
+{
+    public class User
+    {
+        protected string _name="";
+        public string Name { get { return _name; } set { _name = value; } }
+        public User(string name)
+        {
+            Name = name;
+        }
+        public User() {
+            Name = "";
+        }
+    }
+}
